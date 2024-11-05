be600c278c3b83dd8ea73cec8687acde28a6402b
nuneung1612 <gyugg21@gmail.com>
2024-11-05 21:46:56 +0700

    edit fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
     if num % 3 == 0:
         return "Fizz"
     elif num % 5 == 0:
         return "Buzz"
-    elif num % 3 == 0 and num % 5 == 0:
-        return "FizzBuzz"
     else:
         return num
